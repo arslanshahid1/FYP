6982a4f1aa0b6b29e2baa66227670e4179658cf6
Arslan Shahid <arslanshahid1@gmail.com>
2021-12-03 12:47:10 +0500

    done

--- Frontend/fyp-front/src/App.js
+++ Frontend/fyp-front/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Menu from './components/Menu'
 import Footer from './components/Footer'
+import Packages from './components/Packages'
 
 import {
   BrowserRouter as Router,
@@ -24,6 +25,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path="/menu" element={<Menu/>}/>
+          <Route path="/packages" element={<Packages/>}/>
          
          
         </Routes>
---
+++ Frontend/fyp-front/src/components/Packages.js
@@ -0,0 +1,105 @@
+import React from 'react'
+
+function Packages() {
+    return (
+        <div className="packages container">
+            <h2 className="text-center mb-0 mt-5 diamond"><img src="images/diamond.svg" width="20px" alt="" srcset="" /></h2>
+            <h1 className="text-center mb-5 display-4">Wedding Menu</h1>
+
+            <div className="row">
+                <div className="col-sm-6 col-md-4 mt-3 d-flex align-items-stretch">
+                    <div className="col-12 wm-proposal p-5">
+                        <h3>WM Proposal 1</h3>
+                        <h6 className="font-weight-bold">Price per head: Rs 700 Only</h6>
+                            <ul>
+                                <li>Chicken Korma / Chicken Karahi</li>
+                                <li>Chicken Biryani / Chicken Pulao</li>
+                                <li>Roghni Naan</li>
+                                <li>Raita</li>
+                                <li>Fresh green salad</li>
+                                <li>Dessert (1 from selection)</li>
+                            </ul>
+                        <button className="btn btn-dark">Book Now</button>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-md-4 mt-3 d-flex align-items-stretch">
+                    <div className="col-12 wm-proposal p-5">
+                        <h3>WM Proposal 2</h3>
+                        <h6 className="font-weight-bold">Price per head: Rs 900 Only</h6>
+                            <ul>
+                                <li>Chicken Korma / Chicken Achari</li>
+                                <li>Mutton Biryani / Mutton Pulao</li>
+                                <li>Roghni Naan</li>
+                                <li>Raita</li>
+                                <li>Fresh green salad</li>
+                                <li>Dessert (1 from selection)</li>
+                            </ul>
+                        <button className="btn btn-dark">Book Now</button>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-md-4 mt-3 d-flex align-items-stretch">
+                    <div className="col-12 wm-proposal p-5">
+                        <h3>WM Proposal 3</h3>
+                        <h6 className="font-weight-bold">Price per head: Rs 800 Only</h6>
+                            <ul>
+                                <li>Mutton Korma</li>
+                                <li>Chicken Biryani / Kashmiri Pulao</li>
+                                <li>Roghni Naan</li>
+                                <li>Raita</li>
+                                <li>Fresh green salad</li>
+                                <li>Dessert (1 from selection)</li>
+                            </ul>
+                        <button className="btn btn-dark">Book Now</button>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-md-4 mt-3 d-flex align-items-stretch">
+                    <div className="col-12 wm-proposal p-5">
+                        <h3>WM Proposal 4</h3>
+                        <h6 className="font-weight-bold">Price per head: Rs 850 Only</h6>
+                            <ul>
+                                <li>Mutton Karahi</li>
+                                <li>Chicken Biryani / Chicken Pulao</li>
+                                <li>Roghni Naan</li>
+                                <li>Raita</li>
+                                <li>Fresh green salad</li>
+                                <li>Dessert (1 from selection)</li>
+                            </ul>
+                        <button className="btn btn-dark">Book Now</button>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-md-4 mt-3 d-flex align-items-stretch">
+                    <div className="col-12 wm-proposal p-5">
+                        <h3>WM Proposal 5</h3>
+                        <h6 className="font-weight-bold">Price per head: Rs 1000 Only</h6>
+                            <ul>
+                                <li>Chicken Achari / Chicken Karahi</li>
+                                <li>Puri & Halwa Suji / Pathoora</li>
+                                <li>Channa Bhaji</li>
+                                <li>Roghni Naan</li>
+                                <li>Raita</li>
+                                <li>Fresh green salad</li>
+                            </ul>
+                        <button className="btn btn-dark">Book Now</button>
+                    </div>
+                </div>
+                <div className="col-sm-6 col-md-4 mt-3 d-flex align-items-stretch">
+                    <div className="col-12 wm-proposal p-5">
+                        <h3>WM Proposal 6</h3>
+                        <h6 className="font-weight-bold">Price per head: Rs 1100 Only</h6>
+                            <ul>
+                                <li>Puri or Pathoora, Channa, Bhajji</li>
+                                <li>Halwa Sujji</li>
+                                <li>Chicken Sheekh Kebab / Chicken Tikka Boti</li>
+                                <li>Roghni Naan</li>
+                                <li>Raita</li>
+                                <li>Fresh green salad</li>
+                            </ul>
+                        <button className="btn btn-dark">Book Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Packages
